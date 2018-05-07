99ced6b4c35ef11b02e0f49409ff1533ff889674
UtkuBelemir <utkubelemir@hotmail.com.tr>
2018-05-07 13:50:16 +0300

    CORS Changed

--- bikriptAPI/main.go
+++ bikriptAPI/main.go
@@ -13,7 +13,9 @@ func NewBikriptHandlers() *BikriptHandlers{
 	return bkHand
 }
 func SetCORS(wri http.ResponseWriter){
+	//TODO : EN SON KULLANILMAYAN REQUEST TYPELARI Düzelt
 	wri.Header().Set("Content-Type", "application/json; charset=utf-8")
+	wri.Header().Set("Access-Control-Allow-Methods", "*")
 	wri.Header().Set("Access-Control-Allow-Origin", "*")
 	wri.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization")
 }
